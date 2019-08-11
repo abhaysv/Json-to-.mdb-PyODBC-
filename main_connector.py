08757e183786846970407eb8bcdf36fb7439711e
Abhay SV <monuabhay@yahoo.in>
2019-08-11 20:00:23 +0530

    Logger Added + Fixed Json Element Loop

--- main_connector.py
+++ main_connector.py
@@ -1,21 +1,52 @@
+#####################################################################################
+#                                                                                   #
+# ▒█▀▀▀█ ▒█░░▒█ ▒█▄░▒█ ▒█▀▀█ 　 ▒█▀▀▄ ░█▀▀█ ▒█▀▀▀ ▒█▀▄▀█ ▒█▀▀▀█ ▒█▄░▒█              #
+# ░▀▀▀▄▄ ▒█▄▄▄█ ▒█▒█▒█ ▒█░░░ 　 ▒█░▒█ ▒█▄▄█ ▒█▀▀▀ ▒█▒█▒█ ▒█░░▒█ ▒█▒█▒█              #
+# ▒█▄▄▄█ ░░▒█░░ ▒█░░▀█ ▒█▄▄█ 　 ▒█▄▄▀ ▒█░▒█ ▒█▄▄▄ ▒█░░▒█ ▒█▄▄▄█ ▒█░░▀█              #
+#                                                                                   #
+#                                                                                   #
+#                                                                                   #
+#   MariaDB --> JSON --> Connector --> PYDOBC(.mdb) --> TrueCAFE                    #
+#                                  --> MS SQL Serv  --> ZkAccess                    #
+#                                                                                   #
+#                                                                                   #
+#####################################################################################
 import pyodbc,datetime,time
 import urllib.request as urllib
 
+#---------------------[APP LOGGING]--------------------------------------------------
+import logging.config
+from datetime import datetime
+
+logging.config.fileConfig('logger.conf')
+logger = logging.getLogger('MainLogger')
+
+fh = logging.FileHandler('{:%Y-%m-%d}.log'.format(datetime.now()))
+formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(lineno)04d | %(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.debug("THE DAEMON RUN INITIAED")
+
+#------------------------------------------------------------------------------------
+
+#--------------------[FOR API]-------------------------------------------------------
 import json,ssl
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+api_key='abhay'
+#------------------------------------------------------------------------------------
 
-# set up some constants
-# set up some constants
 
+#---------------------[Connect to TrueCafe .mdb PYODBC Driver]-----------------------
 MDB = r'E:\GITHUB\Json to .mdb (PyODBC )\TrueCafe.mdb'
 DRV = '{Microsoft Access Driver (*.mdb)}'
 PWD = 'pw'
-
-# connect to db
 con = pyodbc.connect(r'DRIVER={};DBQ={};PWD={}'.format(DRV,MDB,PWD))
+#------------------------------------------------------------------------------------
 
+#---------------------[Fetch Last SYNC Param]----------------------------------------
 cur = con.cursor()
 SQL1='''
 SELECT MAX(mname)
@@ -26,7 +57,7 @@ rc=cur.fetchall()
 print (rc[0][0])
 
 link_id=rc[0][0]
-api_key='abhay'
+
 #url='http://localhost/fabui/api/item?key=abhay&id={}'.format(rc[0][0])
 url='http://localhost/fabui/api/item?key=' + api_key + '&id=' + link_id
 print(url)
@@ -35,9 +66,32 @@ a=urllib.urlopen(url,context=ctx).read().decode()
 b=json.loads(a)
 
 today_date = time.strftime("%m/%d/%Y")
-print(today_date)
-#dt=(float(b[0]["id"]),datetime.datetime.strptime(today, "%m/%d/%Y"),b[0]["email"],b[0]["name"],b[0]["cf1"],b[0]["company"])
-print(b[0]["email"])
+for ele in b:
+  
+
+  insert_query ='r_service_plan,balance,balance_min,access_vector,max_stations,r_user,customer_type,active,r_owner,payment,status,color,expire_date,min_balance_sign,expiry_type,promo,terminal_type,traffic_balance,traffic_max,minutes,bdate,nick,fname,mname,a_password,description'
+  values_dynamic ="'"+ today_date +"','"+ ele['email']+"','"+ ele['name'] + "','"+ ele['id'] +"','"+ ele['cf1'] +"','"+  ele['company']+"'"
+  values_static = " Values(7,0,0,'00000000000001111100000000000000000000000000000000000000000000000000000000000000000000000000000000003',0,3,1,'Y',2,1,100,536870912,'09-10-2020','N',1,'N',0,0,0,5000," + values_dynamic + ");"
+  sql_querry = "INSERT INTO client (" + insert_query + ")" + values_static
+
+  #print(sql_querry)
+#   #cur.execute(sql_querry)
+#   #cur.commit()
+#   SQL1="SELECT MAX(recno) FROM client"
+#   cur.execute(SQL1)
+#   recno=cur.fetchall()
+#   #print(recno[0][0])
+#   SQL2="UPDATE client SET id={} WHERE recno={}".format(recno[0][0],recno[0][0])
+#   print(SQL2)
+#   #cur.execute(SQL2)
+#   #cur.commit()
+#   i+=1
+#   #print("GAND LAG GAYO")
+#   if not b[i]["id"]:
+#     break
+
+
+
 
 
 # ATTRIBUTE LIST 
@@ -64,26 +118,3 @@ print(b[0]["email"])
 # traffic balance-0
 # traffic_max -0
 # minutes-5000
- 
-
-insert_query ='r_service_plan,balance,balance_min,access_vector,max_stations,r_user,customer_type,active,r_owner,payment,status,color,expire_date,min_balance_sign,expiry_type,promo,terminal_type,traffic_balance,traffic_max,minutes,bdate,nick,fname,mname,a_password,description'
-
-values_dynamic ="'"+ today_date +"','"+ b[0]['email']+"','"+ b[0]['name'] + "','"+ b[0]['id'] +"','"+ b[0]['cf1'] +"','"+  b[0]['company']+"'"
-
-
-values_static = " Values(7,0,0,'00000000000001111100000000000000000000000000000000000000000000000000000000000000000000000000000000003',0,3,1,'Y',2,1,100,536870912,'09-10-2020','N',1,'N',0,0,0,5000," + values_dynamic + ");"
-sql_querry = "INSERT INTO client (" + insert_query + ")" + values_static
-
-cur.execute(sql_querry)
-cur.commit()
-SQL1="SELECT MAX(recno) FROM client"
-
-cur.execute(SQL1)
-recno=cur.fetchall()
-print(recno[0][0])
-SQL2="UPDATE client SET id={} WHERE recno={}".format(recno[0][0],recno[0][0])
-cur.execute(SQL2)
-cur.commit()
-
-
-
